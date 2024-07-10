c32a9527bfb3ed31df3f2ac026c7844f9d7971f5
khurram44 <ks7844201@gmail.com>
2024-07-10 18:24:37 +0500

    slider

---
+++ app.configs.js
@@ -0,0 +1,5 @@
+export const variables = {
+    baseOrign: window.location.origin,
+    scopeOrigin:"U2FsdGVkX19IFcUPFt1UJ3/0WodMap1OmE5GsuBmEnKU5PpcpYSc4nKJBrk5dVgxazguX0+4ym4dW7fYyg46Dg==",
+    scopeOrigins:"U2FsdGVkX18F6+2F/HEYglxPgxAShWtm4FDyg89lRhyLJKWCyNvb6RQqZeFkZo/h"
+}
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'development', // Change this to 'production' for production builds
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+  },
+};
