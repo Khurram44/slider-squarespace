b51eef9e14233f2e7a23227cb2c92dc6e8182051
khurram44 <ks7844201@gmail.com>
2024-07-12 23:43:24 +0500

    change_colot

--- widget.js
+++ widget.js
@@ -44,36 +44,36 @@ const Widget = () => {
       num: "01",
       title: "Create Brand Profile",
       desc: "Register your company and industry details",
-      image:
-        "https://images.squarespace-cdn.com/content/65bccc6ee546664e7822619e/cea7662b-8fb9-4d92-8c5b-e4ab83c3cb68/S1+Create+Profile.png?content-type=image%2Fpng",
+      image:"https://www.calconic.com/user/themes/calconic/images/step-1.webp"
+        // "https://images.squarespace-cdn.com/content/65bccc6ee546664e7822619e/cea7662b-8fb9-4d92-8c5b-e4ab83c3cb68/S1+Create+Profile.png?content-type=image%2Fpng",
     },
     {
       num: "02",
       title: "Select your Package",
       desc: "Select calculator pack and customise your calculators with your brand colours",
-      image:
-        "https://images.squarespace-cdn.com/content/65bccc6ee546664e7822619e/a335ec86-edfb-43a1-99a2-f15035900835/S2+Select+Package.png?content-type=image%2Fpng",
+      image:"https://www.calconic.com/user/themes/calconic/images/step-1.webp"
+        // "https://images.squarespace-cdn.com/content/65bccc6ee546664e7822619e/a335ec86-edfb-43a1-99a2-f15035900835/S2+Select+Package.png?content-type=image%2Fpng",
     },
     {
       num: "03",
       title: "Set-up Payment Method",
       desc: "Complete your subscription payment details",
-      image:
-        "https://images.squarespace-cdn.com/content/65bccc6ee546664e7822619e/43eca5d4-c8c6-4d16-95c4-709dbdda78e7/S3+Setup+Payment.png?content-type=image%2Fpng",
+      image:"https://www.calconic.com/user/themes/calconic/images/step-1.webp"
+        // "https://images.squarespace-cdn.com/content/65bccc6ee546664e7822619e/43eca5d4-c8c6-4d16-95c4-709dbdda78e7/S3+Setup+Payment.png?content-type=image%2Fpng",
     },
     {
       num: "04",
       title: "Package customisation",
       desc: "We style your calculators so that it seamlessly fits into your website design.",
-      image:
-        "https://images.squarespace-cdn.com/content/65bccc6ee546664e7822619e/79b94d1c-9cdd-4b2a-99cc-84264d9cd9c7/S4+Customisation.png?content-type=image%2Fpng",
+      image:"https://www.calconic.com/user/themes/calconic/images/step-1.webp"
+        // "https://images.squarespace-cdn.com/content/65bccc6ee546664e7822619e/79b94d1c-9cdd-4b2a-99cc-84264d9cd9c7/S4+Customisation.png?content-type=image%2Fpng",
     },
     {
       num: "05",
       title: "Download API Code",
       desc: "Embed a customised calculator to your website and start generating leads for your sales team.",
-      image:
-        "https://images.squarespace-cdn.com/content/65bccc6ee546664e7822619e/79b94d1c-9cdd-4b2a-99cc-84264d9cd9c7/S4+Customisation.png?content-type=image%2Fpng",
+      image:"https://www.calconic.com/user/themes/calconic/images/step-1.webp"
+        // "https://images.squarespace-cdn.com/content/65bccc6ee546664e7822619e/79b94d1c-9cdd-4b2a-99cc-84264d9cd9c7/S4+Customisation.png?content-type=image%2Fpng",
     },
   ];
 
@@ -92,7 +92,7 @@ const Widget = () => {
   };
 
   return (
-    <div className="slider-container" style={{ backgroundColor: "#3B454E" }}>
+    <div className="slider-container" style={{ backgroundColor: "#0e3242" }}>
       <Slider {...settings}>
         {data.map((item, index) => (
           <div key={index} className="slide">
